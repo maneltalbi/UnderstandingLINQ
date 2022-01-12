ef3f7672077f344463e3ed838d8c4979c44c5fe0
manel talbi <talbimanel20@gmail.com>
2022-01-12 13:54:09 +0100

    using LINQ query

---
+++ UnderstandingLINQ1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+
+namespace UnderstandingLINQ
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> myCars = new List<Car>()
+            {
+            new Car() { VIN="A1" , Make ="BMW" , Model= "550i" , StickerPrice = 55000 , Year = 2005 },
+            new Car() { VIN = "B2", Make = "Toyota", Model = "4Runner", StickerPrice = 35000, Year = 2000 },
+            new Car() { VIN = "C3", Make = "BMW", Model = "745li", StickerPrice = 75000, Year = 2012 },
+            new Car() { VIN = "D4", Make = "Ford", Model = "Escape", StickerPrice = 25000, Year = 2004 },
+            new Car() { VIN = "E5", Make = "BMW", Model = "55i", StickerPrice = 57000, Year = 2010 }
+
+            };
+            //Link query
+            var bmws = from car in myCars
+                       where car.Make == "BMW"
+                       && car.Year == 2010
+                       select car;
+
+            //Link method
+            foreach (var car in bmws)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
+
+
+    }
+
+}
